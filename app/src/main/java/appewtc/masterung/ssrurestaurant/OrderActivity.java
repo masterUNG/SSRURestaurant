f598830ab8613d5508a61eecd57e366409df3217
masterUNG <phrombutr@gmail.com>
2015-05-21 15:14:07 +0700

    Syn Food

--- app/src/main/java/appewtc/masterung/ssrurestaurant/OrderActivity.java
+++ app/src/main/java/appewtc/masterung/ssrurestaurant/OrderActivity.java
@@ -1,9 +1,24 @@
 package appewtc.masterung.ssrurestaurant;
 
+import android.os.Build;
+import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class OrderActivity extends ActionBarActivity {
@@ -12,7 +27,80 @@ public class OrderActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
-    }
+
+        //Syn JSON to SQLite
+        synJSONtoSQLite();
+
+    }   // onCreate
+
+    private void synJSONtoSQLite() {
+
+        //Setup Policy
+        if (Build.VERSION.SDK_INT > 9) {
+
+            StrictMode.ThreadPolicy myPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(myPolicy);
+
+        }   // if
+
+        InputStream objInputStream = null;
+        String strJSON = "";
+
+        //Create InputStream
+        try {
+
+            HttpClient objHttpClient = new DefaultHttpClient();
+            HttpPost objHttpPost = new HttpPost("http://swiftcodingthai.com/ssru1/php_get_data_food.php");
+            HttpResponse objHttpResponse = objHttpClient.execute(objHttpPost);
+            HttpEntity objHttpEntity = objHttpResponse.getEntity();
+            objInputStream = objHttpEntity.getContent();
+
+        } catch (Exception e) {
+            Log.d("ssru", "Create InputStream ==> " + e.toString());
+        }
+
+
+        //Create strJSON
+        try {
+
+            BufferedReader objBufferedReader = new BufferedReader(new InputStreamReader(objInputStream, "UTF-8"));
+            StringBuilder objStringBuilder = new StringBuilder();
+            String strLine = null;
+
+            while ((strLine = objBufferedReader.readLine()) != null ) {
+                objStringBuilder.append(strLine);
+            }   //while
+
+            objInputStream.close();
+            strJSON = objStringBuilder.toString();
+
+
+        } catch (Exception e) {
+            Log.d("ssru", "Create strJSON ==> " + e.toString());
+        }
+
+
+        //Update Value to SQLite
+        try {
+
+            final JSONArray objJsonArray = new JSONArray(strJSON);
+            for (int i = 0; i < objJsonArray.length(); i++) {
+
+                JSONObject jsonObject = objJsonArray.getJSONObject(i);
+                String strFood = jsonObject.getString("Food");
+                String strPrice = jsonObject.getString("Price");
+                FoodTABLE objFoodTABLE = new FoodTABLE(this);
+                objFoodTABLE.addFood(strFood, strPrice);
+
+            }   // for
+
+        } catch (Exception e) {
+            Log.d("ssru", "Update SQlite ==> " + e.toString());
+        }
+
+
+
+    }   // synJSON
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,4 +123,4 @@ public class OrderActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+}   // Main Class
